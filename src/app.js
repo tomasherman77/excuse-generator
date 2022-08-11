5cd31b799cbb439b93fd9e79fa5f126859feac2f
tomasherman77 <tomasxherman@gmail.com>
2022-08-11 17:40:34 +0000

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#motivo").addEventListener("click", () => {
+    document.querySelector("#motivo").innerHTML = excusa();
+  });
+};
+
+const excusa = function() {
+  let first = who[Math.floor(Math.random() * 4)];
+  let second = action[Math.floor(Math.random() * 4)];
+  let third = what[Math.floor(Math.random() * 4)];
+  let fourth = when[Math.floor(Math.random() * 4)];
+  return first + " " + second + " " + third + " " + fourth;
 };
